2ea1cf1630d682665f4812c63d8052d6a82f35cb
Daniel Zamanillo <dzamanillok@gmail.com>
2021-11-03 08:54:16 -0500

    built

---
+++ client/build/precache-manifest.1b04325924f1a04475f3f412c1db768d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a1769576dc3370149967e4172a308a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f42c4eae514076d58516",
+    "url": "/static/css/main.dc614c6b.chunk.css"
+  },
+  {
+    "revision": "755779e93629d2a310a8",
+    "url": "/static/js/2.9ab84755.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.9ab84755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f42c4eae514076d58516",
+    "url": "/static/js/main.daa13951.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
